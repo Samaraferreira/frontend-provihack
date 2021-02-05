19a60d566c762d25c3cbd252321e23f5807f776f
Samara Ferreira <ferreirasamara.sf@gmail.com>
2021-02-04 21:42:48 -0300

    chore: config eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
+function App () {
   return (
     <div className="App">
+      Hello
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
